397d87036ad5e71dacbff49d88d26043a1ed55d5
rafizul896 <rafizulislam896@gmail.com>
2025-03-10 10:24:18 +0600

    add: login form and functionality

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "@/components/modules/auth/login/LoginForm";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
---
+++ src/components/modules/auth/login/LoginForm.tsx
@@ -0,0 +1,97 @@
+"use client";
+import Logo from "@/app/assets/svgs/Logo";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { loginUser } from "@/services/AuthService";
+import { toast } from "sonner";
+import { LoginSchema } from "./LoginValidation";
+
+const LoginForm = () => {
+  const form = useForm({ resolver: zodResolver(LoginSchema) });
+
+  const {
+    formState: { isSubmitting },
+  } = form;
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    console.log(data);
+    try {
+      const res = await loginUser(data);
+      if (res?.success) {
+        toast.success(res?.message);
+      } else {
+        toast.error(res?.message);
+      }
+      console.log(res);
+    } catch (err: any) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="border-2 border-gray-300 rounded-xl flex-grow max-w-md w-full p-5">
+      <div className="flex items-center space-x-4 ">
+        <Logo />
+        <div className="mb-3">
+          <h1 className="text-xl font-semibold">Login</h1>
+          <p className="font-extralight text-sm text-gray-600">
+            Welcome back!
+          </p>
+        </div>
+      </div>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button type="submit" className="mt-5 w-full">
+            {isSubmitting ? "Logging...." : "Login"}
+          </Button>
+        </form>
+      </Form>
+      <p className="text-sm text-gray-600 text-center my-3">
+        Do not have any account ?
+        <Link href="/register" className="text-primary">
+          Register
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default LoginForm;

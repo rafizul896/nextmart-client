1c6ad3d9bb0da6dc668768032acf07d58e3fd2b0
rafizul896 <rafizulislam896@gmail.com>
2025-03-10 10:05:19 +0600

    add: User Registration with Server Actions

---
+++ src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "@/components/modules/auth/register/RegisterForm";
+
+const RegisterPage = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <RegisterForm />
+    </div>
+  );
+};
+
+export default RegisterPage;

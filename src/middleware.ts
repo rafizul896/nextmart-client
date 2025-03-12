275534c20a0e654ba1c22096273e0256b7207c2e
rafizul896 <rafizulislam896@gmail.com>
2025-03-12 14:07:06 +0600

    add: Secure Routes with Middleware

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getCurrentUser } from "./services/AuthService";
+
+const authRoutes = ["/login", "/register"];
+
+export const middleware = async (request: NextRequest) => {
+  const userInfo = await getCurrentUser();
+  const { pathname } = request.nextUrl;
+
+  if (!userInfo) {
+    if (authRoutes.includes(pathname)) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(
+        new URL(
+          `http://localhost:3000/login?redirectPath=${pathname}`,
+          request.url
+        )
+      );
+    }
+  }
+};
+
+export const config = {
+  matcher: ["/create-shop"],
+};

7763000d7893484a77a93e0b014dd1329b63659e
rafizul896 <rafizulislam896@gmail.com>
2025-03-14 22:41:25 +0600

    update: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,19 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getCurrentUser } from "./services/AuthService";
 
-type TRole = keyof typeof roleBasedPrivateRoutes;
+type Role = keyof typeof roleBasedPrivateRoutes;
 
 const authRoutes = ["/login", "/register"];
 
 const roleBasedPrivateRoutes = {
-  user: [/^\/user/],
+  user: [/^\/user/, /^\/create-shop/],
   admin: [/^\/admin/],
 };
 
 export const middleware = async (request: NextRequest) => {
-  const userInfo = await getCurrentUser();
   const { pathname } = request.nextUrl;
+
+  const userInfo = await getCurrentUser();
 
   if (!userInfo) {
     if (authRoutes.includes(pathname)) {
@@ -27,15 +28,23 @@ export const middleware = async (request: NextRequest) => {
     }
   }
 
-  if (userInfo?.role && roleBasedPrivateRoutes[userInfo?.role as TRole]) {
-    const routes = roleBasedPrivateRoutes[userInfo?.role as TRole];
-
+  if (userInfo?.role && roleBasedPrivateRoutes[userInfo?.role as Role]) {
+    const routes = roleBasedPrivateRoutes[userInfo?.role as Role];
     if (routes.some((route) => pathname.match(route))) {
       return NextResponse.next();
     }
   }
+
+  return NextResponse.redirect(new URL("/", request.url));
 };
 
 export const config = {
-  matcher: ["/create-shop"],
+  matcher: [
+    "/login",
+    "/create-shop",
+    "/admin",
+    "/admin/:page",
+    "/user",
+    "/user/:page",
+  ],
 };

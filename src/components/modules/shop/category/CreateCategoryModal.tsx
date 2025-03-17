4ed4f83abf4b3ee254c9f86a18eaeb766b9b1a7f
rafizul896 <rafizulislam896@gmail.com>
2025-03-17 14:34:51 +0600

    add: Modify Sidebar and Add Create Category Modal

---
+++ src/components/modules/shop/category/CreateCategoryModal.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+const CreateCategoryModal = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Create Category</Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Create Product Category</DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateCategoryModal;

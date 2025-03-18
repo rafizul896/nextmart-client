a9cd139ef078cdd428373e10b92c891b670dab71
rafizul896 <rafizulislam896@gmail.com>
2025-03-18 22:24:57 +0600

    add: deleteModal

--- src/types/category.type.ts
+++ src/types/category.type.ts
@@ -2,7 +2,12 @@ export interface ICategory {
   _id: string;
   name: string;
   description: string;
+  parent: string | null;
   icon: string;
+  slug: string;
   isActive: boolean;
   createdBy: string;
+  createdAt: string;
+  updatedAt: string;
+  children: ICategory[];
 }
